053d34e0824665496283ff92003acd1076f5653c
maneth909 <hoeunmaneth09@gmail.com>
2026-01-11 21:53:43 +0700

    cookies handling

--- middleware.ts
+++ middleware.ts
@@ -33,7 +33,7 @@ export async function middleware(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  // 5. Protected Route Logic
+  // Protected Route Logic
   if (!user && request.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
