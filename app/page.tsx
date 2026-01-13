1199486a29f0a2912c9855313534a87c273ad64d
maneth909 <hoeunmaneth09@gmail.com>
2026-01-13 11:56:28 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,14 @@
 import Button from "../components/Button";
 import ThemeToggle from "../components/ThemeToggle";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-6 p-10">
-      <div className="flex flex-wrap gap-4 items-center">
-        <p className="text-base">Test buttons</p>
-        {/* Solid Primary (Default) */}
-        <Button>Save Changes</Button>
-
-        {/* Outline */}
-        <Button variant="outline">Edit Profile</Button>
-
-        {/* Secondary (Grey) */}
-        <Button variant="secondary">Cancel</Button>
-
-        {/* Sizes */}
-        <Button size="sm">Small</Button>
-        <Button size="lg">Large</Button>
-
-        {/* Disabled */}
-        <Button disabled>Disabled</Button>
-
-        {/* Loading State */}
-        <Button isLoading>Processing...</Button>
-      </div>
-
+      <h1 className="text-4xl font-bold">Welcome to AI Resume Builder</h1>
+      <Link href="/login">
+        <Button>Go to Login Page to get started</Button>
+      </Link>
       <ThemeToggle />
     </div>
   );
